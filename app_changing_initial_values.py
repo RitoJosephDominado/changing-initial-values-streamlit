5a9e6220c9114e18770882b29b0a91a62c54ca3a
Rito Dominado <ritojosephdominado@gmail.com>
2024-08-21 14:18:52 +0800

    separated plot_species_across_initial_values function into an updating and a plotting function. added number_inputs to select the list of initial values

--- app_changing_initial_values.py
+++ app_changing_initial_values.py
@@ -12,16 +12,22 @@ st.set_page_config(layout="wide")
 
 file = 'crntoolbox_results_aug_15/legewi_wild_higher_deficiency.rtf'
 
-def plot_species_accross_initial_values(start_point, end_point, num_points, 
-                                        initial_value_df, rate_df, species_observed, 
-                                        species_initial_value_changed, varied_initial_values):
+def update_ivp_df_list(
+        start_point, end_point, num_points,
+        initial_value_df, rate_df,
+        species_initial_value_changed, varied_initial_values
+):
+    iv_df = initial_value_df.copy(deep = True)
     df_list = []
     for iv in varied_initial_values:
-        initial_value_df.loc[initial_value_df['species'] == species_initial_value_changed, 'concentration'] = iv
-        sol = Solver(initial_value_df, rate_df)
+        iv_df.loc[iv_df['species'] == species_initial_value_changed, 'concentration'] = iv
+        sol = Solver(iv_df, rate_df)
         sol.solve(num_points, start_point, end_point)
         df_list.append(sol.get_solution_df())
+    st.session_state['ivp_df_list'] = df_list
 
+def plot_species_across_initial_values(species_observed):
+    df_list = st.session_state.ivp_df_list
     vec_list = [df_list[x].loc[:, species_observed] for x in range(len(df_list))]
     fig, ax = plt.subplots(figsize=(15, 8))
     for x in vec_list:
@@ -44,6 +50,9 @@ if 'initial_value_df' not in st.session_state:
 if 'rate_df' not in st.session_state:
     st.session_state['rate_df'] = extract_rate_df(file)
 
+if 'ivp_df_list' not in st.session_state:
+    st.session_state['ivp_df_list'] = []
+
 col1, col2, col3 = st.columns(3)
 with col1:
     st.header('Rate constants')
@@ -58,24 +67,46 @@ with col3:
     st.table(st.session_state.steady_state_df)
 
 row2_col1, row2_col2, row2_col3, row2_col4 = st.columns(4)
-with row2_col1:
-    species_observed = st.selectbox('Species to Observe', st.session_state.initial_value_df.species, index=9)
-    st.write('You selected to observe: ', species_observed)
 
-with row2_col2:
+with row2_col1:
     species_initial_value_changed = st.selectbox('Species with Changing Initial Values', st.session_state.initial_value_df.species, index=3)
 
-st.header('Results')
+with row2_col2:
+    starting_initial_value = st.number_input('Range Start', min_value=0, value=0)
+with row2_col3:
+    ending_initial_value = st.number_input('Range End', min_value = 0, value=200)
+with row2_col4:
+    num_initial_values = st.number_input('Number of Initial Values', min_value=0, value=5)
+
+varied_initial_values = np.linspace(
+    starting_initial_value, ending_initial_value,
+    num_initial_values
+)
+st.write('Initial Values to Use:')
+st.write(str(varied_initial_values))
 
 start_point = 0
 end_point = 20
 num_points = 21
-initial_value_df = st.session_state.initial_value_df
-rate_df = st.session_state.rate_df
-varied_initial_values = np.arange(0, 250, 50)
 
+run_btn = st.button('Run', on_click=update_ivp_df_list, args = (
+    start_point, end_point, num_points,
+    st.session_state.initial_value_df, st.session_state.rate_df,
+    species_initial_value_changed, varied_initial_values
+))
 
-st.pyplot(plot_species_accross_initial_values(
-    start_point, end_point, num_points, 
-    initial_value_df, rate_df, species_observed,
-    species_initial_value_changed, varied_initial_values)[0])
+st.header('Results')
+
+if len(st.session_state['ivp_df_list']) > 0:
+    iv_df = st.session_state.initial_value_df.copy(deep = True)
+    species_observed = st.selectbox('Species to Observe', iv_df.species, index=9)
+    st.write('You selected to observe: ', species_observed)
+    st.pyplot(plot_species_across_initial_values(species_observed)[0])
+    ivp_results_dict = {}
+    for i, x in enumerate(varied_initial_values):
+        ivp_results_dict[species_initial_value_changed + '=' + str(x)] = st.session_state['ivp_df_list'][i].loc[:, species_observed]
+    ivp_results_df = pd.DataFrame(ivp_results_dict)
+    st.table(ivp_results_df)
+    st.divider()
+    st.write('Initial Values used')
+    st.table(st.session_state.initial_value_df)
